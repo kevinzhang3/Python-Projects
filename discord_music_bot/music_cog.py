5e97e31633f4611befcf387f6e74d0905b26347d
Kevin <kzhang366@gmail.com>
2024-07-13 16:10:34 -0600

    started implementing commands

--- discord_music_bot/music_cog.py
+++ discord_music_bot/music_cog.py
@@ -29,7 +29,8 @@ class music_cog(commands.cog):
                 return False
             return {'source': vid['formats'][0]['url'], 'title': vid['title']}
         
-    def play_next(self):
+    def skip(self):
+        # base case to stop recursion
         if len(self.queue > 0):
             self.playing = True
 
@@ -39,7 +40,38 @@ class music_cog(commands.cog):
             # dequeue the song
             self.queue.pop(0)
 
-            # play the song in vc using FFmpeg and the options defined, after the song finishes playing, call play_next again
-            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda x: self.play_next())
+            # play the song in vc using FFmpeg and the options defined, after the song finishes playing, call play_next again recursively
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda x: self.skip())
         else:
             self.playing = False
+
+    async def start(self, msg):
+        # check for songs
+        if len(self.queue) > 0:
+
+            self.playing = True
+            m_url = self.queue[0][0]['source']
+
+            # check if bot is currently in vc
+            if self.vc == None or not self.vc.is_connected():
+                # attempt to join vc
+                self.vc = await self.queue[0][1].connect()
+
+                # send error message if failed to join vc
+                if self.vc == None:
+                    await msg.send("Could not connect to the voice channel.")
+                    return
+            # move from one vc to another
+            else:
+                await self.vc.move_to(self.queue[0][1])
+
+            # dequeue and play song
+            self.queue.pop(0)
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda x: self.skip())
+
+        else:
+            self.playing = False
+
+    @commands.command(name='play', aliases=["p"], help='Play song from YouTube.')
+    async def play(self, msg, *args):
+        query = " ".join(args)
