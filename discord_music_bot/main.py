fa0b5bbe2011b55d08f1841c9d919e35a2021ac3
Kevin <kzhang366@gmail.com>
2024-07-13 15:10:11 -0600

    began implementation of music functions

--- discord_music_bot/main.py
+++ discord_music_bot/main.py
@@ -1,4 +1,14 @@
 import discord
 from discord.ext import commands
+import os
 
-bot = commands.Bot(command_prefix="/")
+from help_cog import help_cog
+from music_cog import music_cog
+
+# makes it so all commands must be prefixed with "m."
+bot = commands.Bot(command_prefix="m.")
+
+bot.add_cog(music_cog)
+bot.add_cog(help_cog)
+
+bot.run(os.getenv("token"))
--- discord_music_bot/music_cog.py
+++ discord_music_bot/music_cog.py
@@ -6,3 +6,40 @@ from youtube_dl import YoutubeDL
 class music_cog(commands.cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.playing = False
+        self.paused = False
+
+        # music queue
+        self.queue = []
+
+        # best audio quality, and no playlists (does not work with bot)
+        self.YDL_OPTS = {'format': 'bestaudio', 'noplaylist': 'True'}
+
+        # only deal with audio
+        self.FFMPEG_OPTIONS = {'options': '-vn'}
+
+        self.vc = None
+
+    def search(self, item):
+        with YoutubeDL(self.YDL_OPTS) as ydl:
+            try:
+                vid = ydl.extract_info(f"ytsearch:{item}", download=False)['entries'][0]
+            except Exception:
+                return False
+            return {'source': vid['formats'][0]['url'], 'title': vid['title']}
+        
+    def play_next(self):
+        if len(self.queue > 0):
+            self.playing = True
+
+            # the url of the song to by played taken from the first element of the queue
+            m_url = self.queue[0][0]['source']
+
+            # dequeue the song
+            self.queue.pop(0)
+
+            # play the song in vc using FFmpeg and the options defined, after the song finishes playing, call play_next again
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda x: self.play_next())
+        else:
+            self.playing = False
