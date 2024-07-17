95553f4fa0a6ad25359e05a1de6995d9a444b1fb
Kevin <kzhang366@gmail.com>
2024-07-16 22:40:13 -0600

    continue implementation of help_cog

--- discord_music_bot/help_cog.py
+++ discord_music_bot/help_cog.py
@@ -9,7 +9,13 @@ class help_cog(commands.Cog):
 ```
 Commands:
 m.help - displays all commands
-m.p <song> searches for the given song in youtube and plays it in the channel you are currently in.
+m.p <song> searches for the given song in youtube and plays it in the channel you are currently
+m.q - displays all songs currently in queue
+m.skip - skips the current song
+m.clear - clears the music queue
+m.leave - disconnect muse from the voice channel
+m.pause - pause whatever is currently playing
+m.resume - resume music
 ```
-
 """
+        self.text_channel = []
