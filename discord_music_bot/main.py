d7095996c4f756095ce2169b346ff351dc392b0d
Kevin <kzhang366@gmail.com>
2024-07-22 13:43:29 -0600

    fixes

--- discord_music_bot/main.py
+++ discord_music_bot/main.py
@@ -1,12 +1,15 @@
 import discord
 from discord.ext import commands
-import os
+import os, asyncio
 
+# import cogs
 from help_cog import help_cog
 from music_cog import music_cog
 
+i = discord.Intents.all()
+
 # makes it so all commands must be prefixed with "m."
-bot = commands.Bot(command_prefix="m.")
+bot = commands.Bot(command_prefix="m.", intents=i)
 
 bot.remove_command("help")
 
--- discord_music_bot/music_cog.py
+++ discord_music_bot/music_cog.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from youtube_dl import YoutubeDL
 
 # this cog class will be assigned to the bot and will serve as the functionality/implementation of searching and playing music
-class music_cog(commands.cog):
+class music_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
